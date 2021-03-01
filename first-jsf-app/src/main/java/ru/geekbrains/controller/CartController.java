afd1d1061a898f78b953e62ff9fc98b7dc643861
Roman Russkikh <r-ru@yandex.ru>
2021-03-01 19:25:02 +0300

    Lesson 4. Устранил мелкие недоработки. Вынес панель навигации и скрипты в отдельные блоки html-кода.

--- first-jsf-app/src/main/java/ru/geekbrains/controller/CartController.java
+++ first-jsf-app/src/main/java/ru/geekbrains/controller/CartController.java
@@ -5,19 +5,19 @@ import ru.geekbrains.persist.Product;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Named
 @SessionScoped
 public class CartController implements Serializable {
 
-    private Map<Long, Product> cart = new HashMap<>();
+    private final Map<Long, Product> cart = new HashMap<>();
 
     public List<Product> getProductsInCart() {
-        return cart.entrySet().stream().map(entry -> entry.getValue()).collect(Collectors.toList());
+        return new ArrayList<>(cart.values());
     }
 
     public void addToCart(Product product) {
