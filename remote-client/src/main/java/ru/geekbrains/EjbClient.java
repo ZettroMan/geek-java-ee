8612cf8734c4eed023c4ec6f7b3b773309735b29
Roman Russkikh <r-ru@yandex.ru>
2021-03-09 17:40:57 +0300

    Lesson 6. Вторая попытка с удаленным клиентом...

--- remote-client/src/main/java/ru/geekbrains/EjbClient.java
+++ remote-client/src/main/java/ru/geekbrains/EjbClient.java
@@ -8,21 +8,19 @@ import javax.naming.NamingException;
 import java.io.IOException;
 import java.util.Properties;
 
-public class Test {
+public class EjbClient {
     public static void main(String[] args) throws IOException, NamingException {
         Context context = createInitialContext();
 
         String jndiServiceName = "ejb:/first-jsf-app/ProductServiceImpl!ru.geekbrains.services.ProductServiceRemote";
         ProductServiceRemote productService = (ProductServiceRemote) context.lookup(jndiServiceName);
 
-        productService.findAll();
-
-//                .forEach(prod -> System.out.println(prod.getId() + "\t" + prod.getName() + "\t" + prod.getPrice()));
+        productService.findAll()
+                .forEach(prod -> System.out.println(prod.getId() + "\t" + prod.getName() + "\t" + prod.getPrice()));
     }
 
     public static Context createInitialContext() throws IOException, NamingException {
         final Properties env = new Properties();
-        env.load(Test.class.getClassLoader().getResourceAsStream("wildfly-jndi.properties"));
+        env.load(EjbClient.class.getClassLoader().getResourceAsStream("wildfly-jndi.properties"));
         return new InitialContext(env);
-    }
-}
+    }}
